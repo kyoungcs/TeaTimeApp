12fdffd2a1e29b08f42b5f0e570d8086f6bd9aae
Ryson <rysonsynic@att.net>
2015-11-11 00:25:44 -0800

    made changes to origional TeaTime, can be safely ignored and will be delt with. Use TeaTimeRemake
    have working app and two floating action buttons. the one for server
    testing have comments to follow

---
+++ TeaTimeRemake/app/src/main/java/sonoma/teatimeremake/CalendarActivity.java
@@ -0,0 +1,168 @@
+package sonoma.teatimeremake;
+
+import android.os.Bundle;
+import java.util.GregorianCalendar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import sonoma.teatimeremake.adapter.CalendarAdapter;
+import sonoma.teatimeremake.util.CalendarCollection;
+
+
+public class CalendarActivity extends AppCompatActivity {
+    public GregorianCalendar cal_month, cal_month_copy;
+    private CalendarAdapter cal_adapter;
+    private TextView tv_month;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        cal_month = (GregorianCalendar) GregorianCalendar.getInstance();
+        cal_month_copy = (GregorianCalendar) cal_month.clone();
+        //cal_adapter = new CalendarAdapter(this, cal_month, CalendarCollection.date_collection_arr);
+
+        tv_month = (TextView) findViewById(R.id.tv_month);
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
+
+        ImageButton previous = (ImageButton) findViewById(R.id.ib_prev);
+
+        previous.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //this is going to show the previous month
+                setPreviousMonth();
+                refreshCalendar();
+            }
+        });
+
+        ImageButton next = (ImageButton) findViewById(R.id.Ib_next);
+        next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //this is going to show next month
+                setNextMonth();
+                refreshCalendar();
+
+            }
+        });
+
+
+        GridView gridview = (GridView) findViewById(R.id.gv_calendar);
+        gridview.setAdapter(cal_adapter);
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v, position);
+                String selectedGridDate = CalendarAdapter.day_string
+                        .get(position);
+
+                String[] separatedTime = selectedGridDate.split("-");
+                String gridvalueString = separatedTime[2].replaceFirst("^0*", "");
+                int gridvalue = Integer.parseInt(gridvalueString);
+
+                if ((gridvalue > 10) && (position < 8)) {
+
+                    setPreviousMonth();
+                    refreshCalendar();
+                } else if ((gridvalue < 7) && (position > 28)) {
+                    setNextMonth();
+                    refreshCalendar();
+                }
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v, position);
+
+                ((CalendarAdapter) parent.getAdapter()).getPositionList(selectedGridDate, CalendarActivity.this);
+            }
+
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "This is for the adding elements.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        FloatingActionButton tester = (FloatingActionButton) findViewById(R.id.tester);
+        tester.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                do the stuff you want to do in here, probably befor the pop-up
+                */
+                Snackbar.make(view, "This is for testing server-side.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    protected void setNextMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month
+                .getActualMaximum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) + 1),
+                    cal_month.getActualMinimum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) + 1);
+        }
+
+    }
+
+    protected void setPreviousMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month
+                .getActualMinimum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) - 1),
+                    cal_month.getActualMaximum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) - 1);
+        }
+
+    }
+
+    public void refreshCalendar() {
+        cal_adapter.refreshDays();
+        cal_adapter.notifyDataSetChanged();
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_calendar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ TeaTimeRemake/app/src/main/java/sonoma/teatimeremake/util/CalendarCollection.java
@@ -0,0 +1,19 @@
+package sonoma.teatimeremake.util;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Austin on 11/10/2015.
+ */
+public class CalendarCollection {
+    public String date="";
+    public String event_message = "";
+
+    public static ArrayList<CalendarCollection> date_collection_arr;
+    public  CalendarCollection(String date, String event_message){
+
+        this.date = date;
+        this.event_message=event_message;
+
+    }
+}
