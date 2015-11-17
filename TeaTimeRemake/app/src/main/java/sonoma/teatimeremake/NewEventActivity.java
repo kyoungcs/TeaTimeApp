b652c16b040e5e95c553751a679eca21f23873e6
Ryson <rysonsynic@att.net>
2015-11-17 15:49:50 -0800

    setting up somethings for this to work
    basic set-up to have day events instead of pop-up like previously

--- TeaTimeRemake/app/src/main/java/sonoma/teatimeremake/NewEventActivity.java
+++ TeaTimeRemake/app/src/main/java/sonoma/teatimeremake/NewEventActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import sonoma.teatimeremake.util.CalendarCollection;
 
@@ -11,22 +12,32 @@ public class NewEventActivity extends AppCompatActivity implements View.OnClickL
 
     Button eventButton;
     String date, eventMessage;
+    //for adding hours+mins
+    int time;
+    private EditText inputMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
 
+        inputMessage = (EditText)findViewById(R.id.eventMessage);
+
         eventButton = (Button) findViewById(R.id.eventButton);
         eventButton.setOnClickListener(this);
     }
 
 
+
     public void onClick(View v){
         switch(v.getId()){
             case R.id.eventButton:
+
                 date = "2015-11-26";
+                //at moment, unused on purpose
+                time = 0;
                 eventMessage = "Thanksgiving";
+                //need to add time to CalendarCollection
                 CalendarCollection.date_collection_arr.add(new CalendarCollection(date, eventMessage));
 
             default:
